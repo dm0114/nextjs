0e220e7875573fdf4ca4d01f5ac24101a818da86
dm0114 <dm10802@gmail.com>
2022-09-05 14:37:04 +0900

    pjt02

---
+++ project02/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+export default function FilteredEventPage() {
+    return (
+        <div>
+            <h1>
+                Filtered event page!
+            </h1>
+        </div>
+    )
+}
---
+++ project02/pages/index.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>
+                Starting page
+            </h1>
+        </div>
+    )
+}
